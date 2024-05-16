c0bc635f66ea848d387e8dd6f2e61c2df5676be8
chacachien <nguyentienphatbl123@gmail.com>
2024-05-16 16:04:05 +0700

    data store

--- app/chatbot/ragBot/data.py
+++ app/chatbot/ragBot/data.py
@@ -34,18 +34,7 @@ class LoadData:
         self.docsearch = None
         self.PC = None
 
-
-
     def load_data(self):
-        # loader = GoogleDriveLoader(
-        #     folder_id = self.folder_id,
-        #     token_path = 'token.json',
-        #     recusive = False,
-        # )
-        # self.docs = loader.load()
-
-        # data = self.db.run('SELECT name, intro, content FROM mdl_page where course=4')
-        # print("DATA: ", data)
 
         data_clear = []
         from bs4 import BeautifulSoup
@@ -71,6 +60,9 @@ class LoadData:
 
         merged_string = ''.join(data_clear)
         self.docs = merged_string
+        # store into file txt
+        with open('app/chatbot/ragBot/new.txt', 'w') as file:
+            file.write(str(merged_string))
         return merged_string
 
     def split_data(self):
@@ -122,7 +114,7 @@ def main():
     #     file.write(str(res))
     
     # data.split_data()
-    search = data.embed_data()
+    # search = data.embed_data()
     # data.update_data()
     query = "Cuộc chiến tranh nhà Ngô"
 
@@ -133,7 +125,38 @@ def main():
     # res = data.docsearch.as_retriever(search_type='mmr')
     # mat = res.invoke(query)
     # pretty_print_docs(mat)
-    res = data.docsearch.max_marginal_relevance_search(query, k=3, fetch_k=10)
-    pretty_print_docs(res)
+    # res = data.docsearch.max_marginal_relevance_search(query, k=3, fetch_k=10)
+    # pretty_print_docs(res)
+
+    index = data.pc.Index(name=data.index_name)
+    # res = index.fetch(['c42951d4-f3fa-4893-9f22-ad9674ff50e2'])
+
+    # #convert res to json
+    # import json
+    # res = json.loads(res)
+
+    # # get the keys of this json
+    # keys = res.keys()
+    # print(keys)
+
+    # update data
+    # res = index.update(
+    #     id = 'c42951d4-f3fa-4893-9f22-ad9674ff50e2',
+    #     set_metadata= {'title': 'Tác giả', 'text':'(Hồ Xuân Hương, Bà Huyện Thanh Quan, Đoàn Thị Điểm, Ngọc Hân Công Chúa), có những đóng góp vô cùng quan trọng cho văn học chữ Nôm, có rất nhiều tác phẩm văn học chữ Nôm, có khá nhiều nghiên cứu về các tác phẩm văn học chữ Nôm khác, góp phần cho sự phát triển hưng thịnh của văn học chữ Nôm. Trong giai đoạn phát triển chữ Quốc Ngữ và chữ Nôm, chữ Nôm đã được sử dụng trong các tác phẩm thơ ca trào phúng khác nhau, thể hiện thói hóm hỉnh vốn có của người Việt.Lục Vân TiênLà tác phẩm nổi tiếng nhất của Nguyễ``n Đình Chiểu, Lục Vân Tiên không chỉ là một tác phẩm văn học chữ Nôm ca ngợi lòng trung hiếu của một đấng nam nhi, sự vận hành của âm-dương, cũng như câu nói \"qua cơn bỉ cực đến hồi thái lai\" cũng được thể hiện rõ nét trong tác phẩm. Lý tưởng tin tưởng vào sự vận hành của vũ trụ, của quy luật nhân quả đã được Nguyễn Đình Chiểu khéo léo thể hiện để cuộc đời chàng Lục Vân Tiên tuy thăng trầm nhưng cũng đến được bến bờ hạnh phúc.Tỳ Bà TruyệnĐược chuyển thể từ một vở kịch Trung Hoa là Tỳ Bà Ký, Tỳ Bà Truyện là một thể loại văn học chữ Nôm dạng truyện thơ, một thể loại khá nổi tiếng trong giới văn chương đương thời. Tỳ Bà Truyện là một câu chuyện ngụ ngôn mang tính giáo huấn, đề cao cái đẹp thủy chung, tận tâm của người con dâu tên Ngũ Nương. Trải qua bao thăng trầm bể dâu, cô nhất định làm tròn trách nhiệm người con dâu, cũng vì thế mà được người đời ngưỡng mộ vì đức hy sinh và sự tận tụy của mình.Quan Âm Thị KínhKhác với tác phẩm chèo cùng tên, truyện thơ Quan Âm Thị Kính vốn là một tác phẩm văn học chữ Nôm mang đề tài tôn giáo để kể về nguồn gốc của Phật Bà Quan Âm Nghìn Mắt Nghìn Tay. Trong tác phẩm truyện thơ Quan Âm Thị Kính, nhân vật chính là công chúa Diệu Thiên, vì lòng từ bi quyết chí tu hành, bà đã trở thành Quan Âm. Nhiều lần nàng chịu chết thay vì từ bỏ đức tin, lòng từ bi ấy đã bao trùm cả vong hồn dưới âm phủ. Cuối cùng, bà chấp nhận bị cắt đứt tay mình để cứu phụ thân, người luôn tìm cách hãm hại bà.'}
+    # )
+    # print(res)
+    # des = data.pc.describe_index('langchain-demo1')
+    # print(des)
+
+    res = index.query(
+            vector=[0.1]*768,
+        filter = {
+            'course': 1
+        },
+        top_k=1,
+        include_metadata=True
+    )
+    print(res)
+
 if __name__ == "__main__":
     main()
