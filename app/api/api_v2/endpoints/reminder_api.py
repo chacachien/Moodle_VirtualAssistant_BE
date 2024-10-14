edde29a9636e16a80e10b275f1fb966d5e8b04e0
nquduy65 <nguyenquocduy06051998@gmail.com>
2024-10-14 20:07:14 +0700

    add pusher and use it in reminder

--- app/api/api_v2/endpoints/reminder_api.py
+++ app/api/api_v2/endpoints/reminder_api.py
@@ -5,26 +5,38 @@ from sqlalchemy.sql.annotation import Annotated
 from app.chatbot.taskbot.reminder import Reminder
 from app.models.reminder_model import RemiderContent
 
+import pusher
+
+pusher_client = pusher.Pusher(
+    app_id='1879635',
+    key='9de03240cc8a5c22c658',
+    secret='b82363250a9736265d17',
+    cluster='ap1',
+    ssl=True
+)
+
 router = APIRouter()
 
 reminderBot = Reminder()
+
+
 @router.post("/reminder")
-async def get_reminder(reminder: RemiderContent ):
-    #user =auth_wrapper(3)  # Passing chatId to the auth wrapper
+async def get_reminder(reminder: RemiderContent):
+    # user =auth_wrapper(3)  # Passing chatId to the auth wrapper
     # if user == 0:
     #     raise HTTPException(status_code=401, detail="Invalid token")
-    #### Create the message: Phat
-    #message = reminderBot.create_content_reminder(reminder)
+    # Create the message: Phat
+    message = reminderBot.create_content_reminder(reminder)
 
-    ### end create message
+    # end create message
 
-    ### Template data
+    # Template data
     user_id = 1
     content = "A quiz has been added for u."
 
-    ### Send to user: DUY
-        ### Set push event to user here
+    # Send to user: DUY
+    # Set push event to user here
+    pusher_client.trigger('moodle-remind', f"{user_id}", {'message': f"{message}"})
 
-
-    ### End sen to user
+    # End sen to user
     return message
