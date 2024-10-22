e57529dcc1f8df6b341ef30b8b777f91d5efbdfe
chacachien <nguyentienphatbl123@gmail.com>
2024-10-22 22:18:33 +0700

    adjust deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 cd befastapi/Moodle_VirtualAssistant_BE
 git pull origin main
 source venv/bin/activate
-pkill -f uvicorn
+pkill -f "uvicorn.*--port 5001"
+sleep 3
 nohup uvicorn app.main:app --host 0.0.0.0 --reload --port 5001 &
