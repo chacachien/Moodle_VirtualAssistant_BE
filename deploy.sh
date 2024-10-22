3ff3e6d8a8bb53c505e057afcd9e2e42586b197f
EC2 Default User <ec2-user@ip-172-30-0-226.ap-southeast-1.compute.internal>
2024-10-22 16:10:40 +0000

    temp

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ git pull origin main
 source venv/bin/activate
 pkill -f "uvicorn.*--port 5001"
 sleep 3
-nohup uvicorn app.main:app --host 0.0.0.0 --reload --port 5001 &
+nohup uvicorn app.main:app --host 0.0.0.0 --port 5001 &
